ab93cb8bffd7523a860e64e0a7ce75fa55241eb2
dudurb17 <eduardo.robettibedin@gmail.com>
2024-04-03 10:11:41 -0300

    filament

---
+++ app/Filament/Resources/PostResource.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PostResource\Pages;
+use App\Filament\Resources\PostResource\RelationManagers;
+use App\Filament\Resources\PostResource\RelationManagers\CategoriesRelationManager;
+use App\Models\Post;
+use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PostResource extends Resource
+{
+    protected static ?string $model = Post::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                ->required()
+                ->maxLength(255),
+                Forms\Components\Textarea::make('content')
+                ->columnSpanFull(),
+                // Select::make('categories')
+                // ->relationship('categories', 'name')
+                // ->multiple(),
+                Forms\Components\FileUpload::make("image")
+                ->image(),
+                Forms\Components\Toggle::make('active')
+                ->required()
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                ->searchable(),
+                Tables\Columns\ImageColumn::make('image')
+                ->circular(),
+                Tables\Columns\ToggleColumn::make('active'),
+                Tables\Columns\TextColumn::make("created_at")
+                ->dateTime()
+                ->sortable()
+                ->toggleable(isToggledHiddenByDefault:true)
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            CategoriesRelationManager::class
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPosts::route('/'),
+            'create' => Pages\CreatePost::route('/create'),
+            'edit' => Pages\EditPost::route('/{record}/edit'),
+        ];
+    }
+}
